9f5f4070a3e0a53e4fbdb9119c43cea59b3282c5
IgorDerik <igorderik@yahoo.com>
2018-02-05 03:05:00 +0200

    added some more queries

--- DataBase/HW1/queries.sql
+++ DataBase/HW1/queries.sql
@@ -1,4 +1,3 @@
-USE labor_sql;
 #1-1
 SELECT maker, type FROM product
 ORDER BY maker;
@@ -26,3 +25,59 @@ ORDER BY speed DESC;
 #1-5
 SELECT name, class FROM ships
 ORDER BY name;
+
+#1-6 
+SELECT * FROM pc
+WHERE speed >= 500 AND price < 800 
+ORDER BY price DESC;
+
+#1-8
+SELECT model, speed FROM pc
+WHERE speed >= 400 AND speed <= 600 
+ORDER BY hd;
+
+
+#1-10
+SELECT model, speed, hd, price FROM laptop 
+WHERE screen >= 12
+ORDER BY price DESC;
+
+#1-11
+SELECT model, type, price FROM printer 
+WHERE price < 300 
+ORDER BY type DESC;
+
+#1-12
+SELECT model, ram, price FROM laptop 
+WHERE ram = 64 
+ORDER BY screen;
+
+#1-13
+SELECT model, ram, price FROM pc
+WHERE ram > 64 
+ORDER BY hd;
+
+#1-17
+SELECT * FROM income 
+WHERE point = 1 
+ORDER BY inc;
+
+#2-1
+SELECT model FROM pc
+WHERE model LIKE '%1%1%';
+
+#2-4
+SELECT name FROM ships 
+WHERE name LIKE 'W%n';
+
+#2-5
+SELECT name	FROM ships 
+WHERE name LIKE '%e%e%';
+
+#2-10
+SELECT date FROM pass_in_trip 
+WHERE place LIKE '1%';
+
+#2-11
+SELECT date FROM pass_in_trip 
+WHERE place LIKE '%c';
